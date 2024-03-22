6f723ab1e02aab97051c8c0306ea8e83214ced92
tivereidoro <tivereidoro@yahoo.com>
2024-03-22 22:45:22 +0100

    Fix bug in Asynch generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,6 +13,6 @@ async def async_generator() -> Generator[float, None, None]:
     wait 1 second, then yield a random number between 0 and 10.
     Use the random module.
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
